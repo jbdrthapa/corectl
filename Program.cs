51e8b75455a00790b5e0fc23cd8fc47900a1ff5e
Jamir Bahadur Thapa <jamir@grub-stealth.HOME>
2020-04-26 15:43:14 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using coreinfo;
+using Grpc.Net.Client;
+
+namespace corectl
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClientHandler = new HttpClientHandler();
+            // Return `true` to allow certificates that are untrusted/invalid
+            httpClientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var httpClient = new HttpClient(httpClientHandler);
+            var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient });
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest{ Name = "GreeterClient" });
+            Console.WriteLine("Greeter: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+    }
+}
