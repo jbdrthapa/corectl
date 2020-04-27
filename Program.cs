9cefdca86f91b4c15f8d4869ad02f601eee3b4c2
Jamir Bahadur Thapa <jamir@grub-stealth.HOME>
2020-04-26 21:02:25 -0700

    Adding CPUInfo call

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Grpc.Net.Client;
 
 namespace Core.Client
 {
@@ -9,13 +7,31 @@ namespace Core.Client
     {
         static async Task Main(string[] args)
         {
+            // Print CPUInfo
+            await PrintCPUInfo();
+
+            Console.WriteLine("Press any key to exit...");
+
+            Console.Read();
+        }
+
+        private static async Task PrintCPUInfo()
+        {
             var cpuInfoClient = ClientsFactory.CpuInfoClient;
 
             var reply = await cpuInfoClient.GetCpuInfoAsync(new Protos.CpuInfoRequest());
 
-            Console.WriteLine("Cpu Model: " + reply.Model);
-            Console.WriteLine("Press any key to exit...");
-            Console.Read();
+            Console.WriteLine($"Cpu Model Name: {reply.ModelName}");
+            Console.WriteLine($"Cpu Model: {reply.Model}");
+            Console.WriteLine($"Cpu Cores: {reply.Cores}");
+            Console.WriteLine($"Cpu Cache: {reply.Cache}");
+            Console.WriteLine($"Cpu Family: {reply.Family}");
+            Console.WriteLine($"Cpu Flags: {reply.Flags}");
+            Console.WriteLine($"Cpu MHz: {reply.MHz}");
+            Console.WriteLine($"Cpu Microcode: {reply.Microcode}");
+            Console.WriteLine($"Cpu Stepping: {reply.Stepping}");
+            Console.WriteLine($"Cpu Vendor: {reply.Vendor}");
+            Console.WriteLine($"Cpu VMX Flags: {reply.VMXFlags}");
         }
     }
 }
