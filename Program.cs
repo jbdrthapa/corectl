0da6e7c040c85fd049ea5d766c577b9a8f209c73
Jamir Bahadur Thapa <jamir@grub-stealth.HOME>
2020-04-26 23:05:23 -0700

    adding streaming support for cpuinfo

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using Core.Protos;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 
 namespace Core.Client
 {
@@ -8,30 +12,60 @@ namespace Core.Client
         static async Task Main(string[] args)
         {
             // Print CPUInfo
-            await PrintCPUInfo();
+            await GetCPUInfoAsync();
+
+            await StreamCPUInfoAsync();
 
             Console.WriteLine("Press any key to exit...");
 
             Console.Read();
         }
 
-        private static async Task PrintCPUInfo()
+        private static async Task StreamCPUInfoAsync()
         {
             var cpuInfoClient = ClientsFactory.CpuInfoClient;
 
-            var reply = await cpuInfoClient.GetCpuInfoAsync(new Protos.CpuInfoRequest());
+            var cancellationTokenSource = new CancellationTokenSource();
 
-            Console.WriteLine($"Cpu Model Name: {reply.ModelName}");
-            Console.WriteLine($"Cpu Model: {reply.Model}");
-            Console.WriteLine($"Cpu Cores: {reply.Cores}");
-            Console.WriteLine($"Cpu Cache: {reply.Cache}");
-            Console.WriteLine($"Cpu Family: {reply.Family}");
-            Console.WriteLine($"Cpu Flags: {reply.Flags}");
-            Console.WriteLine($"Cpu MHz: {reply.MHz}");
-            Console.WriteLine($"Cpu Microcode: {reply.Microcode}");
-            Console.WriteLine($"Cpu Stepping: {reply.Stepping}");
-            Console.WriteLine($"Cpu Vendor: {reply.Vendor}");
-            Console.WriteLine($"Cpu VMX Flags: {reply.VMXFlags}");
+            using var streamingCpuInfo = cpuInfoClient.GetCpuInfoStream(new Empty(), null, null, cancellationTokenSource.Token);
+
+            try
+            {
+                await foreach (var cpuInfoData in streamingCpuInfo.ResponseStream.ReadAllAsync(cancellationToken: cancellationTokenSource.Token))
+                {
+                    await PrintCPUInfoAsync(cpuInfoData);
+                }
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+            {
+                await Console.Out.WriteLineAsync("Cpu Info stream cancelled.");
+            }
+        }
+
+        private static async Task GetCPUInfoAsync()
+        {
+            var cpuInfoClient = ClientsFactory.CpuInfoClient;
+
+            var cpuInfoData = await cpuInfoClient.GetCpuInfoAsync(new Empty());
+
+            await PrintCPUInfoAsync(cpuInfoData);
+        }
+
+        private static async Task PrintCPUInfoAsync(CpuInfoData cpuInfoData)
+        {
+            await Console.Out.WriteLineAsync($"Timestamp: {cpuInfoData.DateTimeStamp}");
+            await Console.Out.WriteLineAsync($"Cpu Model Name: {cpuInfoData.ModelName}");
+            await Console.Out.WriteLineAsync($"Cpu Model: {cpuInfoData.Model}");
+            await Console.Out.WriteLineAsync($"Cpu Cores: {cpuInfoData.Cores}");
+            await Console.Out.WriteLineAsync($"Cpu Cache: {cpuInfoData.Cache}");
+            await Console.Out.WriteLineAsync($"Cpu Family: {cpuInfoData.Family}");
+            await Console.Out.WriteLineAsync($"Cpu Flags: {cpuInfoData.Flags}");
+            await Console.Out.WriteLineAsync($"Cpu MHz: {cpuInfoData.MHz}");
+            await Console.Out.WriteLineAsync($"Cpu Microcode: {cpuInfoData.Microcode}");
+            await Console.Out.WriteLineAsync($"Cpu Stepping: {cpuInfoData.Stepping}");
+            await Console.Out.WriteLineAsync($"Cpu Vendor: {cpuInfoData.Vendor}");
+            await Console.Out.WriteLineAsync($"Cpu VMX Flags: {cpuInfoData.VMXFlags}");
+
         }
     }
 }
