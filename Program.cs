921e9f1748783b2581e71d307f09610b71427b60
Jamir Bahadur Thapa <jamir@grub-stealth.HOME>
2020-04-26 17:45:54 -0700

    Converting greet to CpuInfo proto

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using coreinfo;
 using Grpc.Net.Client;
 
-namespace corectl
+namespace Core.Client
 {
     class Program
     {
@@ -15,10 +14,11 @@ namespace corectl
             httpClientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
             var httpClient = new HttpClient(httpClientHandler);
             var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient });
-            var client = new Greeter.GreeterClient(channel);
+            
+            var client = new Core.Protos.CpuInfo.CpuInfoClient(channel);
 
-            var reply = await client.SayHelloAsync(new HelloRequest{ Name = "GreeterClient" });
-            Console.WriteLine("Greeter: " + reply.Message);
+            var reply = await client.GetCpuInfoAsync(new Protos.CpuInfoRequest());
+            Console.WriteLine("Cpu Model: " + reply.Model);
             Console.WriteLine("Press any key to exit...");
             Console.Read();
         }
